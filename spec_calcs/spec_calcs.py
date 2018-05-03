97b45cfd3f93d48361279e5a4311283b3d761bdd
Paul La Plante <plaplant@sas.upenn.edu>
2018-05-03 09:36:13 -0700

    Add BDA functions to spec_calcs

--- spec_calcs/spec_calcs.py
+++ spec_calcs/spec_calcs.py
@@ -67,7 +67,8 @@ def decorrelations(max_decorr=0.1, frequency=(250. * 1e6 * units.Hz),
                    n_channels=default_n_channels, chan_width=(250 * units.MHz) / default_n_channels,
                    integration_time=(.1 * units.s),
                    fringe_stop_int_time=(10 * units.s),
-                   verbose=False, wrap_len=default_wrap_len):
+                   verbose=False, wrap_len=default_wrap_len, decorr_rate='linear',
+                   lx=(100 * units.m), ly=(100 * units.m)):
 
     wavelength = const.c / frequency.to(1 / units.s)
     earth_rot_speed = (Angle(360, units.deg) / units.sday).to(units.arcminute / units.s)
@@ -122,9 +123,15 @@ def decorrelations(max_decorr=0.1, frequency=(250. * 1e6 * units.Hz),
                        format(decorr=max_decorr * 100),
                        corr_FoV_max_decorr.to(units.degree), wrap_len)
 
-    decorr_fringe_stop = ((fringe_stop_int_time * np.sin(corr_FoV.radian) *
-                           earth_rot_speed * abs(np.sin(hera_latitude))) /
-                          max_resolution.to(units.arcminute))
+    if decorr_rate == 'linear':
+        decorr_fringe_stop = ((fringe_stop_int_time * np.sin(corr_FoV.radian) *
+                               earth_rot_speed * abs(np.sin(hera_latitude))) /
+                              max_resolution.to(units.arcminute))
+    elif decorr_rate == 'quadratic':
+        decorr_fringe_stop, max_rfac = decorr_post_fs_int_time_quad(lx, ly, fringe_stop_int_time,
+                                                                    corr_FoV, earth_rot_speed, wavelength)
+        decorrelations['max_rfac'] = max_rfac
+
     decorrelations['decorr_fringe_stop'] = decorr_fringe_stop
     if verbose:
         print_desc_val('Decorrelation due to time integrating after fringe '
@@ -233,3 +240,80 @@ def data_rates(integration_time=(.1 * units.s), fringe_stop_int_time=(10 * units
                                    bytes_per_vis * sum_diff_factor / fringe_stop_int_time))
 
     return Naive_data_rate, post_fringe_stop_data_rate
+
+
+def bda_compression_factor(max_decorr=0.1, frequency=(250. * 1e6 * units.Hz),
+                           baseline=(870 * units.m), corr_FoV=Angle(90., units.degree),
+                           n_channels=default_n_channels, chan_width=(250 * units.MHz) / default_n_channels,
+                           integration_time=(.1 * units.s),
+                           fringe_stop_int_time=(10 * units.s), decorr_rate='linear', lx=(14.6 * units.m),
+                           ly = (14.6 * units.m), verbose=False, wrap_len=default_wrap_len):
+
+    compression_factor = {}
+
+    # calculate the total decorrelation for this given baseline and correlator settings
+    decorr = decorrelations(max_decorr, frequency, baseline, corr_FoV, n_channels, chan_width,
+                            integration_time, fringe_stop_int_time, verbose, wrap_len, decorr_rate=decorr_rate,
+                            lx=lx, ly=ly)
+
+    if decorr['post_fs_total_decorr'] < max_decorr:
+        # if total decorr is less than max, we can average
+        wavelength = const.c / frequency.to(1 / units.s)
+        bl_res = Angle(np.arcsin(wavelength / baseline), units.radian)
+        earth_rot_speed = (Angle(360, units.deg) / units.sday).to(units.rad / units.s)
+        # compute post-fringe-stopping decorrelation allowed based on total value and pre-fringe-stopping contribution
+        post_fs_decorr = 1 - (1 - max_decorr) / (1 - decorr['pre_fs_total_decorr'])
+        # invert post-fringe-stopped decorrelation to find maximum possible integration time
+        if decorr_rate == 'linear':
+            fs_int_time = (post_fs_decorr * bl_res / np.sin(corr_FoV) / earth_rot_speed
+                           / np.abs(np.sin(hera_latitude))).to(units.s).value
+        else:
+            fs_int_time = np.sqrt(6 * post_fs_decorr / (np.pi**2 * decorr['max_rfac']))
+
+        # compute the number of samples that can be averaged using a power-of-two scheme
+        num_ints = np.floor(np.log2(fs_int_time / fringe_stop_int_time.value))
+        fac = 2**(-num_ints)
+        compression_factor['num_ints'] = num_ints
+        compression_factor['compression_factor'] = fac
+    else:
+        # no compression possible
+        compression_factor['num_ints'] = 1
+        compression_factor['compression_factor'] = 1
+
+    return compression_factor
+
+def dudt(lx, ly, ha, ers, wl):
+    ha = max(min(ha, Angle(90., units.degree)), Angle(-90., units.degree))
+    return (lx * np.cos(ha) - ly * np.sin(ha)) * ers / wl
+
+def dvdt(lx, ly, ha, dec, ers, wl):
+    ha = max(min(ha, Angle(90., units.degree)), Angle(-90., units.degree))
+    dec = max(min(dec, Angle(90., units.degree)), Angle(-90., units.degree))
+    return (lx * np.sin(dec) * np.sin(ha) + ly * np.sin(dec) * np.cos(ha)) * ers / wl
+
+def decorr_post_fs_int_time_quad(lx, ly, int_time, corr_FoV, ers, wl):
+    # case 1: +l
+    du = dudt(lx, ly, corr_FoV, ers, wl)
+    l = np.cos(90 * units.deg + corr_FoV)
+    rfac = (du * l)**2
+
+    # case 2: -l
+    du = dudt(lx, ly, -corr_FoV, ers, wl)
+    l = np.cos(90 * units.deg - corr_FoV)
+    rfac = max(rfac, (du * l)**2)
+
+    # case 3: +m
+    dv = dvdt(lx, ly, 0., hera_latitude + corr_FoV, ers, wl)
+    m = np.cos(90 * units.deg + corr_FoV)
+    rfac = max(rfac, (dv * m)**2)
+
+    # case 4: -m
+    dv = dvdt(lx, ly, 0., hera_latitude - corr_FoV, ers, wl)
+    m = np.cos(90 * units.deg - corr_FoV)
+    rfac = max(rfac, (dv * m)**2)
+
+    # make sure we have the right units
+    rfac = rfac.to(units.rad**2 / units.s**2)
+
+    # add other factors; return time and max rfac value
+    return np.pi**2 * (int_time.value)**2 / 6. * rfac.value, rfac.value
