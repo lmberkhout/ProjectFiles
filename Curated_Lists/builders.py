d13dc470067d078cf2cf858070059446e1bb79b3
ddeboer <ddeboer@berkeley.edu>
2019-08-21 07:39:04 -0700

    added nsf output stuff

--- Curated_Lists/builders.py
+++ Curated_Lists/builders.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 from __future__ import print_function
 import six
+import sys
 # ######################################################################
 # Customize output here by                                             #
 #    1 - defining group symbols and provide listing order              #
@@ -17,61 +18,79 @@ LISTING_ORDER = {'*': 'fileorder', '$': 'alphaorder',
 # ######################################################################
 
 
-class Authors:
-    """Read in and format author lists for papers."""
-    def __init__(self, epoch='H1C'):
-        self.filein = 'builders_{}.txt'.format(epoch.upper())
-        self.fileout = self.filein.split('.')[0] + '.tex'
+def make_nice(s):
+    return s.replace('~', ' ')
 
-    def getAuthors(self, filein=None):
-        if filein is not None:
-            self.filein = filein
-        print('Reading ' + self.filein)
+
+class Authors:
+    """
+    Read in and format author lists for papers, or other purposes like NSF annual reports.
+
+    Parameters
+    ----------
+    epoch : str
+        The epoch(s) to use.  It is a csv string.
+    output_type : str
+        Output type to use.  Other tex or csv
+    """
+    def __init__(self, epoch='H1C', output_type='tex'):
+        epoch = epoch.split(',')
+        self.filesin = []
+        self.fileout = 'out'
+        for e in epoch:
+            self.filesin.append('builders_{}.txt'.format(e.upper()))
+            self.fileout = self.fileout + '_{}'.format(e.upper())
+        self.fileout = self.fileout + '.' + output_type
+
+    def getAuthors(self):
         self.authors = {}
         self.affiliationList = []
         self.fileOrder = []
 
-        with open(self.filein, 'r') as f:
-            for line in f:
-                if len(line.strip()) < 2 or line.strip()[0] == '#':
-                    continue
-                if line.strip()[0] == '&':
-                    data = line.strip()[1:].split()
-                    if len(data) == 1:
-                        id_type = data[0]
-                    elif len(data) == 2:
-                        for k in self.authors.keys():
-                            if data[0].lower() in k:
-                                self.authors[k]['ID'][id_type] = data[1]
-                    continue
-                # Affiliations
-                if line[0] == '@':
-                    affiliation = line[1:].strip()
-                    self.affiliationList.append(affiliation)
-                    continue
-                # Names
-                name = line.strip().split()
-                if len(name) != 2 and len(name) != 3:
-                    print("Invalid name format:  {}".format(name))
-                    continue
-                if name[0][0] in GROUP_ORDER:
-                    group = name[0][0]
-                    firstName = name[0][1:]
-                else:
-                    group = '_'
-                    firstName = name[0]
-                lastName = name[-1]
-                middleName = ''
-                if len(name) == 3:
-                    middleName = name[1]
-                akey = lastName.lower() + firstName.lower()
-                if akey in self.authors.keys():
-                    self.authors[akey]['affiliations'].append(affiliation)
-                else:
-                    self.authors[akey] = {'first': firstName, 'middle': middleName, 'last': lastName,
-                                          'affiliations': [affiliation], 'group': group, 'ID': {}}
-                    self.fileOrder.append(akey)
-        self.alphaOrder = sorted(self.authors.keys())
+        for filein in self.filesin:
+            print("Reading " + filein)
+            with open(filein, 'r') as f:
+                for line in f:
+                    if len(line.strip()) < 2 or line.strip()[0] == '#':
+                        continue
+                    if line.strip()[0] == '&':
+                        data = line.strip()[1:].split()
+                        if len(data) == 1:
+                            id_type = data[0]
+                        elif len(data) == 2:
+                            for k in self.authors.keys():
+                                if data[0].lower() in k:
+                                    self.authors[k]['ID'][id_type] = data[1]
+                        continue
+                    # Affiliations
+                    if line[0] == '@':
+                        affiliation = line[1:].strip()
+                        self.affiliationList.append(affiliation)
+                        continue
+                    # Names
+                    name = line.strip().split()
+                    if len(name) != 2 and len(name) != 3:
+                        print("Invalid name format:  {}".format(name))
+                        continue
+                    if name[0][0] in GROUP_ORDER:
+                        group = name[0][0]
+                        firstName = name[0][1:]
+                    else:
+                        group = '_'
+                        firstName = name[0]
+                    lastName = name[-1]
+                    middleName = ''
+                    if len(name) == 3:
+                        middleName = name[1]
+                    akey = lastName.lower() + firstName.lower()
+                    if akey in self.authors.keys():
+                        self.authors[akey]['affiliations'].append(affiliation)
+                    else:
+                        self.authors[akey] = {'first': firstName, 'middle': middleName, 'last': lastName,
+                                              'nice': [make_nice(firstName), make_nice(middleName), make_nice(lastName)],
+                                              'affiliations': [affiliation], 'group': group, 'ID': {}}
+                        self.fileOrder.append(akey)
+            self.alphaOrder = sorted(self.authors.keys())
 
     def setAffiliationNumbers(self):
         self.affilNums = {}
@@ -100,14 +119,11 @@ class Authors:
 
     def niceList(self):
         for aff in self.affiliationList:
-            print('\n' + aff)
+            print('\n-----' + aff)
             for au in self.alphaOrder:
                 for auaff in self.authors[au]['affiliations']:
                     if auaff == aff:
-                        niceFirst = self.authors[au]['first'].replace('~', ' ')
-                        niceMiddle = self.authors[au]['middle'].replace('~', ' ')
-                        niceLast = self.authors[au]['last'].replace('~', ' ')
-                        print("\t{} {} {}".format(niceFirst, niceMiddle, niceLast))
+                        print("{} {} {}".format(self.authors[au]['nice'][0], self.authors[au]['nice'][0], self.authors[au]['nice'][0]))
 
     def group_ordered_list(self):
         self.authororder = []
@@ -121,6 +137,29 @@ class Authors:
                     self.authororder.append(n)
         return self.authororder
 
+    def match_emails(self, contact_csv_file=None):
+        import csv
+        if contact_csv_file is None or not contact_csv_file.endswith('csv'):
+            print("Not matching e-mails [{}]".format(contact_csv_file))
+            return
+
+        print("Matching e-mails with {}".format(contact_csv_file))
+        contacts = {}
+        with open(contact_csv_file) as f:
+            ac = csv.reader(f, delimiter='\t')
+            for row in ac:
+                try:
+                    key = row[0][0].lower() + row[1].lower()
+                except IndexError:
+                    continue
+                contacts[key] = row[4]
+        filast = list(contacts.keys())
+
+        for au in self.alphaOrder:
+            this_filast = self.authors[au]['nice'][0][0].lower() + self.authors[au]['nice'][2].lower()
+            if this_filast in filast:
+                self.authors[au]['email'] = contacts[this_filast]
+
     def get_latex_affiliation(self):
         s = ''
         for k in self.authororder:
@@ -132,9 +171,22 @@ class Authors:
         return s
 
     def write_affil_tex(self, s):
-        print("Writing {}".format(self.fileout))
+        print("Writing tex {}".format(self.fileout))
         with open(self.fileout, 'w') as f:
             f.write(s)
+
+    def write_csv(self):
+        print("Writing csv {}".format(self.fileout))
+        with open(self.fileout, 'w') as f:
+            for au in self.alphaOrder:
+                first = self.authors[au]['nice'][0]
+                middle = self.authors[au]['nice'][1]
+                last = self.authors[au]['nice'][2]
+                if 'email' in self.authors[au].keys():
+                    email = self.authors[au]['email']
+                else:
+                    email = ''
+                print("{},{},{},{}\n".format(first, middle, last, email), file=f, end='')
 
     def get_latex_footnote(self):
         print("This doesn't handle the first affiliation number reliably, unless the first author is from the first affiliation")
@@ -161,17 +213,25 @@ class Authors:
         return s
 
 
+tex_version_types = ['aas', 'footnote']
+csv_version_types = ['nsf']
+epochs = ['H0C', 'H1C', 'H2C']
 if __name__ == '__main__':
     import argparse
     import sys
 
     ap = argparse.ArgumentParser()
-    ap.add_argument('-s', '--screen_only', help="Print list to screen.", action='store_true')
-    ap.add_argument('-e', '--epoch', help="Epoch of builders list (H0C, H1C) [H1C]", default='H1C')
-    ap.add_argument('-v', '--version', help="Version type (aas, footnote) [aas]", default='aas')
+    ap.add_argument('-s', '--screen-only', dest='screen_only', help="Print list to screen.", action='store_true')
+    ap.add_argument('-e', '--epoch', help="Epoch of builders list [H2C]", default='H2C')
+    ap.add_argument('-v', '--version', help="Version type [aas]", choices=tex_version_types + csv_version_types, default='aas')
+    ap.add_argument('--contact-csv-file', dest='contact_csv_file', help="A csv file containing e-mail addresses [None]", default=None)
     args = ap.parse_args()
 
-    h = Authors(args.epoch)
+    if args.version in tex_version_types:
+        output_type = 'tex'
+    else:
+        output_type = 'csv'
+    h = Authors(epoch=args.epoch, output_type=output_type)
     h.getAuthors()
     if args.screen_only:
         h.niceList()
@@ -179,9 +239,13 @@ if __name__ == '__main__':
     if args.version == 'footnote':
         h.setAffiliationNumbers()
     h.group_ordered_list()
+    h.match_emails(contact_csv_file=args.contact_csv_file)
     if args.version == 'footnote':
         s = h.get_latex_footnote()
-    else:
+    elif args.version == 'aas':
         s = h.get_latex_affiliation()
-    h.write_affil_tex(s)
+    if output_type == 'tex':
+        h.write_affil_tex(s)
+    else:
+        h.write_csv()
     h.show_id()
