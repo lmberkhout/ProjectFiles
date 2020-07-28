77dc4d31f6859cb1ab87e89c48a3452873197c0e
David DeBoer <ddeboer@berkeley.edu>
2020-07-28 12:58:31 -0700

    accidently had nodes 6 and 17 twice in antuse

--- show_antuse.py
+++ show_antuse.py
@@ -5,7 +5,7 @@ with open('antuse.json', 'r') as fp:
     data = json.load(fp)
 
 dates = ['20201001', '20201101', '20201201', '20210101', '20210201', '20210301']
-dates += ['20210401', '20210501', '20210601', '20210701', '20210801']
+dates += ['20210401', '20210501', '20210601', '20210701']
 dates.reverse()
 
 color_list = ['b', 'g', 'r', 'c', 'm', 'k', 'tab:blue', 'tab:orange', 'tab:brown', 'tab:olive',
